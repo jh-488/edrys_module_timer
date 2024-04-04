d9beb1bccac96f10e06853758aad8401a0c103dd
jh-488 <jihadhyadi01@gmail.com>
2024-04-04 16:18:19 +0200

    changed folder structure

---
+++ script.js
@@ -0,0 +1,95 @@
+const timerContainer = document.querySelector(".timer_container");
+const feedbackContainer = document.querySelector(".feedback");
+const feedbackMessage = document.getElementById("feedback_message");
+const startBtn = document.getElementById("start");
+const tryAgainBtn = document.getElementById("try_again_btn");
+
+let timerStart;
+
+Edrys.onReady(() => {
+  console.log("Module Timer is loaded!");
+
+  timerStart = Edrys.module.config.timer ? Edrys.module.config.timer : 5;
+});
+
+// Create a countdown timer
+let time;
+let timerInterval;
+
+const countdownElement = document.querySelector(".countdown");
+
+const updateCountdown = () => {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  countdownElement.textContent = `${minutes}:${seconds}`;
+
+  if (time < 0) {
+    clearInterval(timerInterval);
+    time = timerStart * 60;
+    updateCountdown();
+    Edrys.sendMessage("timer-ended", "Timer ended!");
+    startBtn.disabled = false;
+
+    feedbackContainer.classList.add("red-bg");
+    setFeedbackMessage("Time's up! Challenge failed!");
+
+    return;
+  }
+
+  time--;
+};
+
+const stopTimer = () => {
+  clearInterval(timerInterval);
+  time = timerStart * 60;
+  updateCountdown();
+  startBtn.disabled = false;
+};
+
+// Handle the timer start
+const startTimer = () => {
+  startBtn.disabled = true;
+  time = timerStart * 60;
+  updateCountdown();
+  timerInterval = setInterval(updateCountdown, 1000);
+  Edrys.sendMessage("timer-started", "Timer started!");
+};
+
+const tryAgain = () => {
+  feedbackContainer.classList.add("hidden");
+  timerContainer.classList.remove("hidden");
+
+  Edrys.sendMessage("timer-ended", "Timer ended!");
+};
+
+startBtn.addEventListener("click", startTimer);
+tryAgainBtn.addEventListener("click", tryAgain);
+
+// Set the feedback message and show the feedback container
+const setFeedbackMessage = (message) => {
+  feedbackMessage.textContent = message;
+  timerContainer.classList.add("hidden");
+  feedbackContainer.classList.remove("hidden");
+};
+
+// Handle received messages from the code editor module
+Edrys.onMessage(({ from, subject, body, module }) => {
+  if (subject === "challenge-solved") {
+    // Calculate the time needed to solve the challenge
+    elapsedTimeInSeconds = timerStart * 60 - time;
+    const minutes = Math.floor(elapsedTimeInSeconds / 60);
+    const seconds = elapsedTimeInSeconds % 60;
+
+    stopTimer();
+
+    feedbackContainer.classList.add("green-bg");
+    setFeedbackMessage(
+      minutes > 0
+        ? `Congrats! Challenge solved in ${minutes} minutes and ${seconds} seconds!`
+        : `Congrats! Challenge solved in ${seconds} seconds!`
+    );
+  }
+}, (promiscuous = true));
